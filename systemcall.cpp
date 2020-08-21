ca5c61fbc1518eef04243d2a98af570900668845
szokhal <68365653+szokhal@users.noreply.github.com>
2020-08-21 13:38:37 +0900

    systemcall_strcpy

--- systemcall.cpp
+++ systemcall.cpp
@@ -8,3 +8,4 @@ extern EFI_SYSTEM_TABLE *ST;
 bool compare(wchar_t *word1,wchar_t *word2);
 int commandline(wchar_t *com);
 void printf(wchar_t *stringptr);
+void strcpy(wchar_t *word1, wchar_t *word2);
