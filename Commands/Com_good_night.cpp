2e09a74f34595d9b2eaffd669103e86adb47cbf0
Kenbayashi <58835005+Kenbayashi@users.noreply.github.com>
2020-08-23 14:08:23 +0900

    hiをhelloに統合

--- Commands/Com_good_night.cpp
+++ Commands/Com_good_night.cpp
@@ -1,31 +1,31 @@
-  
 #include "systemcall.cpp"
 #include "Command.cpp"
 
-class Com_hello : protected Command {
+class Com_good_night : protected Command {
 private:
-	User * user;
+	User* user;
 
 public:
+	Com_good_night();
 	void makeKeywordList() override;
 	void prepare(User* user) override;
 	void execute() override;
 }
 
-inline Com_hello::Com_hello() {
+inline Com_good_night::Com_good_night() {
 	makeKeywordList();
 }
 
-inline void Com_hello::makeKeywordList() override {
-	plug(&keywords[0], L"good night");
+inline void Com_good_night::makeKeywordList() override {
+	plug(keywords[0], L"good night");
 }
 
-inline void Com_hello::prepare(User* user) override {
+inline void Com_good_night::prepare(User* user) override {
 	this->user = user;
 }
 
-inline void Com_hello::execute() override {
+inline void Com_good_night::execute() override {
 	printf(L"good night ");
-	printf(&(user->name));
+	printf(user->name);
 	printf(L"\r\n");
 }
--- Commands/Com_hello.cpp
+++ Commands/Com_hello.cpp
@@ -1,31 +1,48 @@
 #include "systemcall.cpp"
 #include "Command.cpp"
 
+//クラス名をCom_*****の形に変更する
 class Com_hello : protected Command {
 private:
+	//ユーザーの情報が必要なら書いておく
+	//prepare()内で初期化してから使って
 	User* user;
 
+	//自分で関数を実装する場合はここで宣言
+
 public:
+	//Com_hello()のところは各クラス名に書き換え
+	//それ以外は書き換えなくてOK
 	Com_hello();
 	void makeKeywordList() override;
 	void prepare(User* user) override;
 	void execute() override;
 };
 
+//必須
+//Com_helloのところは各クラス名に書き換え
 inline Com_hello::Com_hello() {
 	makeKeywordList();
 }
 
+//必須
+//20個までキーワードを追加できる
 inline void Com_hello::makeKeywordList() {
 	plug(keywords[0], L"hello");
+	plug(keywords[1], L"hi");
 }
 
+//必須
+//ユーザーの情報が必要なければ中身は空でもOK
+//ユーザーの情報以外のデータがほしいときは要相談
 inline void Com_hello::prepare(User* user) {
 	this->user = user;
 }
 
+//必須
+//実際に実行されるmain()的な部分
 inline void Com_hello::execute() {
-	printf(L"hello ");
+	printf(L"hi ");
 	printf(user->name);
 	printf(L"\r\n");
 }
