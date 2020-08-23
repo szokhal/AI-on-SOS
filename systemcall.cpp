bbfcff4d75bd2a80cc86116cc6c10a0ac6d9c6be
szokhal <68365653+szokhal@users.noreply.github.com>
2020-08-23 13:36:38 +0900

    extern datalist

--- systemcall.cpp
+++ systemcall.cpp
@@ -4,9 +4,11 @@ extern "C" {
 
 #define MAX_COMMAND_LENGTH 100
 
+extern wchar_t[20][10] datalist;
 extern EFI_SYSTEM_TABLE *ST;
 bool compare(wchar_t *word);
 int commandline(wchar_t *com);
 void printf(wchar_t *stringptr);
 void strcpy(wchar_t *word1, wchar_t *word2);
 bool strcmp(wchar_t *word1,wchar_t *word2)
+void filescan();
