0e3a9054cd5f0a64591f6942b5638e37d85ddcb3
szokhal <68365653+szokhal@users.noreply.github.com>
2020-09-14 19:23:16 +0900

    Add files via upload

--- Commands/Com_hello.cpp
+++ Commands/Com_hello.cpp
@@ -1,31 +1,33 @@
-#include "Command.cpp"
+#pragma once
+#include "../User.cpp"
+#include "../systemcall.cpp"
 
-class Com_hello : public Command {
-private:
-	User* user;
+#define MAX_KEYWORD_NUMBER 20
 
-public:
-	Com_hello();
-	void makeKeywordList() override;
-	void prepare(User* user) override;
-	void execute() override;
+struct Com_hello {
+  wchar_t *keywords[MAX_KEYWORD_NUMBER];
+  User user;
+
+  void Com_hello::makeKeywordList() {
+    keywords[0] = L"hello";
+    keywords[1] = L"hi";
+  }
+
+  bool Com_hello::check(wchar_t *com) {
+    for (int i = 0; i < MAX_KEYWORD_NUMBER; i++) {
+      if (strcmps(com, keywords[i]))
+        return true;
+    }
+    return false;
+  }
+
+  void Com_hello::prepare(User user) {
+    this->user = user;
+  }
+
+  void Com_hello::execute() {
+    printfs(L"hi ");
+    printfs(user.name);
+    printfs(L".\r\n");
+  }
 };
-
-inline Com_hello::Com_hello() {
-	makeKeywordList();
-}
-
-inline void Com_hello::makeKeywordList() {
-	plug(keywords[0], L"hello");
-	plug(keywords[1], L"hi");
-}
-
-inline void Com_hello::prepare(User* user) {
-	this->user = user;
-}
-
-inline void Com_hello::execute() {
-	printf(L"hi ");
-	printf(user->name);
-	printf(L"\r\n");
-}
