92af1e5380b9255d72a4b252cad47d603159373a
szokhal <68365653+szokhal@users.noreply.github.com>
2020-09-14 19:20:59 +0900

    Add files via upload

--- User.cpp
+++ User.cpp
@@ -1,22 +1,7 @@
 #pragma once
-#include "systemcall.cpp"
 
-class User {
-public:
-	wchar_t name[20] = L" ";
-	int age = 0;
-	int favorability = 0;
-
-	//�R���X�g���N�^
-	User(wchar_t * newName, int newAge) {
-		strcpy(&name[0], newName);
-		age = newAge;
-	}
-
-	User(wchar_t* newName) {
-		strcpy(&name[0], newName);
-		age = -1;
-	}
-
-	User() {};
+struct User {
+  wchar_t *name = L"kenzo";
+  int age = 0;
+  int favorability = 0;
 };
--- systemcall.cpp
+++ systemcall.cpp
@@ -4,11 +4,11 @@ extern "C" {
 
 #define MAX_COMMAND_LENGTH 100
 
-extern wchar_t[20][10] datalist;
 extern EFI_SYSTEM_TABLE *ST;
+extern unsigned int VerticalResolution, HorisontalResolution;
+bool strcmps(wchar_t *word1,wchar_t *word2);
+void strcpys(wchar_t *word1, wchar_t *word2);
 bool compare(wchar_t *word);
 int commandline(wchar_t *com);
-void printf(wchar_t *stringptr);
-void strcpy(wchar_t *word1, wchar_t *word2);
-bool strcmp(wchar_t *word1,wchar_t *word2);
-void filescan();
+void printfs(wchar_t *stringptr);
+void bootai();
