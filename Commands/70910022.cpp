dade41a6241b334f9b0088d11bbed6298f136a99
Elcia1 <57612225+Elcia1@users.noreply.github.com>
2020-09-20 22:08:48 +0900

    Update 70910022.cpp

--- Commands/70910022.cpp
+++ Commands/70910022.cpp
@@ -11,8 +11,8 @@ struct Com_how_are_you{
   void Com_how_are_you::makeKeywordList() {
     keywords[0] = L"how are you?";
     keywords[1] = L"how are you";
-    keywords[2] = L"how do you feel today?" 
-    keywords[3] = L"how do you feel today"
+    keywords[2] = L"how do you feel today?" ;
+    keywords[3] = L"how do you feel today";
   }
 
   bool Com_hello::check(wchar_t *com) {
