d90b7d3632750e1fcb076df313f2b3f2603624a1
Elcia1 <57612225+Elcia1@users.noreply.github.com>
2020-12-08 16:42:37 +0900

    what language

---
+++ Commands/Com_What_language.cpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "../User.cpp"
+#include "../systemcall.cpp"
+
+#define MAX_WHAT_LANGUAGE_NUMBER 4
+
+static wchar_t what_language_keywords[MAX_WHAT_LANGUAGE_NUMBER][20] = {
+  L"what_language do you speak",
+  L"What language do you speak",
+  L"what_language do you speak?",
+  L"What_language do you speak?"
+};
+
+struct Com_what_language {
+  User user;
+
+  bool Com_what_language::check(wchar_t *com) {
+    for (int i = 0; i < MAX_WHAT_LANGUAGE_NUMBER; i++) {
+      if (strcmps(com, what_language_keywords[i]))
+        return true;
+    }
+    return false;
+  }
+
+  void Com_what_language::prepare(User user) {
+    this->user = user;
+  }
+
+  void Com_what_language::execute() {
+    if(user->favoravility < -50){
+      printfs(L"Who the fuck do you think you are?\n\r");
+    } else if(user->favorability < 50) {
+      printfs(L"I can speak a bit of English.\n\r");
+    }else {
+      printfs(L"I can speak a bit of English.\n\r");
+      printfs(L"I intend to work hard to became able to speak English well!\n\r");
+    }
+  }
+};
