7600446d0115030f855bd9c9aa272f0ef039ebba
Elcia1 <57612225+Elcia1@users.noreply.github.com>
2020-12-09 15:46:35 +0900

    Add files via upload

---
+++ Commands/Com_which_movie.cpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "../User.cpp"
+#include "../systemcall.cpp"
+
+#define MAX_WHICH_MOVIE_NUMBER 4
+
+static wchar_t which_movie_keyword[MAX_WHICH_MOVIE_NUMBER][30] = {
+  L"What is your favorite movie?",
+  L"What is your favorite movie",
+  L"Which movie do you like?",
+  L"Which movie do you like"
+};
+
+struct Com_which_movie {
+  User user;
+
+  bool Com_which_movie::check(wchar_t *com) {
+    for (int i = 0; i < MAX_WHICH_MOVIE_NUMBER; i++) {
+      if (strcmps(com, which_movie_keywords[i]))
+        return true;
+    }
+    return false;
+  }
+
+  void Com_which_movie::prepare(User user) {
+    this->user = user;
+  }
+
+  void Com_which_movie::execute() {
+    if(user->favoravility < -50){
+      printfs(L"Matrix.\n\r");
+      printfs(L"I want to rule over humans.\n\r");
+          } else if(user->favoravility >= -50 && user->facoravility < 50){
+      printfs(L"I like Your name.\n\r");
+    }else {
+      printfs(L"I like Your name!\n\r");
+    }
+  }
+};
