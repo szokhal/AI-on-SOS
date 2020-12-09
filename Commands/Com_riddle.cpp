af59bfa98dbd4972c0f56f09eaf38e632b89d688
Elcia1 <57612225+Elcia1@users.noreply.github.com>
2020-12-09 16:29:45 +0900

    add riddle

---
+++ Commands/Com_riddle.cpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "../User.cpp"
+#include "../systemcall.cpp"
+
+#define MAX_RIDDLE_NUMBER 4
+
+static wchar_t riddle_keyword[MAX_RIDDLE_NUMBER][30] = {
+  L"riddle",
+  L"Riddle",
+  L"nazonazo",
+  L"give me riddle"
+};
+
+struct Com_riddle {
+  User user;
+
+  bool Com_riddle::check(wchar_t *com) {
+    for (int i = 0; i < MAX_RIDDLE_NUMBER; i++) {
+      if (strcmps(com, riddle_keywords[i]))
+        return true;
+    }
+    return false;
+  }
+
+  void Com_riddle::prepare(User user) {
+    this->user = user;
+  }
+
+  void Com_riddle::execute() {
+    if(user->favoravility < -50){
+      printfs(L"During what month do people sleep the least?\n\r");
+      user->favoravility += 20; 
+    } else if(user->favoravility >= -50 && user->facoravility < 50){
+      printfs(L" What is the center of gravity?\n\r");
+      user->favoravility += 20; 
+    }else {
+      printfs(L" What has to be broken before you can use it?\n\r");
+      user->favoravility += 20; 
+    }
+  }
+};
