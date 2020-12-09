b3673181ab8860165f2aa21d35cd36a25d248009
szokhal <68365653+szokhal@users.noreply.github.com>
2020-12-09 14:39:00 +0900

    Add files via upload

--- systemcall.cpp
+++ systemcall.cpp
@@ -4,7 +4,7 @@ extern "C" {
 
 #define MAX_COMMAND_LENGTH 100
 
-extern wchar_t datalist[20][10];
+extern wchar_t datalist[100][100];
 extern EFI_SYSTEM_TABLE *ST;
 extern unsigned int VerticalResolution, HorisontalResolution;
 bool strcmps(wchar_t *word1,wchar_t *word2);
@@ -13,7 +13,7 @@ bool compare(wchar_t *word);
 int commandline(wchar_t *com);
 void printfs(wchar_t *stringptr);
 void bootai();
-void fileread();
-void filewrite();
+void fileread(wchar_t* data, wchar_t* filename);
+void filewrite(wchar_t* data, wchar_t* filename);
 void imageview(wchar_t *imgfilename);
 void scanfs(wchar_t *com);
