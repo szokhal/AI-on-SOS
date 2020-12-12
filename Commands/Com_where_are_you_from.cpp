736ef5cf010da39da00e9cfed9e557f7dc0e90f2
yoru-ni-kakeru <70071548+yoru-ni-kakeru@users.noreply.github.com>
2020-12-12 13:42:25 +0900

    Update Com_where_are_you_from.cpp

--- Commands/Com_where_are_you_from.cpp
+++ Commands/Com_where_are_you_from.cpp
@@ -28,22 +28,28 @@ struct Com_wheredoyoulive {
 
     void Com_wheredoyoulive::execute() {
         if(user->favorability < -50){
+	    imageview(L"despise");
             printfs(L"I won't tell you.");
             printfs(L"\r\n");
         } else if(user->favorability >= -50 && user->favorability < 50){
+	    imageview(L"default2.bgra");
             printfs(L"I live in Japan.");
             printfs(user->name);
             printfs(L".\r\n");
         } else {
+	    imageview(L"smile.bgra");
             printfs(L"I live in Hamamatsu");
             printfs(user->name);
             printfs(L" How about you \r\n");
             if(compare(L"Hamamatsu")||compare(L"Shizuoka")){
+		imageview(L"surprise.bgra");
                 printfs(L"Me too!!\r\n");
             } else if(compare(L"Japan")) {
+		imageview(L"default.bgra");
                 printfs(L"Where do you live in Japan?");
             }else{
-				printfs(L"Oh,that's a good place ");
+		imageview(L"big_smile.bgra");
+		printfs(L"Oh,that's a good place ");
 			}
         }
     }
