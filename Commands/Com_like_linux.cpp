72543091a57d98babeffbd8b8b7044a37a87db1c
yoru-ni-kakeru <70071548+yoru-ni-kakeru@users.noreply.github.com>
2020-12-13 13:08:15 +0900

    Update Com_like_linux.cpp

--- Commands/Com_like_linux.cpp
+++ Commands/Com_like_linux.cpp
@@ -27,12 +27,15 @@ struct Com_like_linux {
 
   void Com_like_linux::execute() {
     if(user->favorability < -50){
+      imageview(L"jotome.bgra");
       printfs(L"Linux is a good one.\n\r");
       user->favorability += 10; 
     } else if(user->favorability >= -50 && user->favorability < 50){
+      imageview(L"smile.bgra");
       printfs(L"Linux is my favorite too!\n\r");
       user->favorability += 20; 
     }else {
+      imageview(L"love.bgra");
       printfs(L"Linux is my favorite too!\n\r");
       printfs(L"I was made by linux.\n\r");
       user->favorability += 30; 
