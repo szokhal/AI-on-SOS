0c06d63737f1db49b7a676ef72ba465de01fb8ad
bassy0909 <71808290+bassy0909@users.noreply.github.com>
2020-12-13 12:58:02 +0900

    Add files via upload

---
+++ Commands/Com_what_anime_do_you_like.cpp
@@ -0,0 +1,41 @@
+#pragma once
+#inlcude <stdio.h>
+#include "../User.cpp"
+#include "../systemcall.cpp"
+
+#define WHATANIMEDOYOULIKE 1
+
+static wchar_t WHATANIMEDOYOULIKE_keywords[MAX_WHATANIMEDOYOULIKE_NUMBER][30] = {
+	L"what anime do you like"
+};
+
+struct Com_WHATANIMEDOYOULIKE {
+  User * user;
+
+  bool Com_WHATANIMEDOYOULIKE::check(wchar_t *com) {
+    for (int i = 0; i < MAX_WHATANIMEDOYOULIKE_NUMBER; i++) {
+      if (strcmps(com, WHATANIMEDOYOULIKE_keywords[i]))
+        return true;
+    }
+    return false;
+  }
+
+  void Com_WHATANIMEDOYOULIKE::prepare(User * user) {
+    this->user = user;
+  }
+
+  void Com_WHATANIMEDOYOULIKE::execute() {
+    if(user->favoravility < 0){
+        printfs(L"I don't like watching anime.");
+				imageview(L"jotome.bgra");
+        printfs(L"\r\n");
+    }else{
+			printfs(L"I like SAO.");
+			imageview(L"big_smile.bgra");
+			printfs(L"\r\n");
+		}
+		user->favoravility+=10;
+		}
+	}
+	imageview(L"default.bgra");
+};
