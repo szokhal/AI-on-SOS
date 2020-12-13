b4dca70e5e77706c2af9e24043013319dcb208eb
Elcia1 <57612225+Elcia1@users.noreply.github.com>
2020-12-13 13:54:57 +0900

    university

--- Commands/Com_university.cpp
+++ Commands/Com_university.cpp
@@ -4,14 +4,14 @@
 
 #define MAX_UNIVERSITY_NUMBER 4
 
-static wchar_t university_keywords[MAX_UNIVERSITY_NUMBER][48] = {
+static wchar_t university_keyword[MAX_UNIVERSITY_NUMBER][30] = {
   L"about university",
   L"university",
   L"Which university do you belong to?"
 };
 
 struct Com_university {
-  User* user;
+  User user;
 
   bool Com_university::check(wchar_t *com) {
     for (int i = 0; i < MAX_UNIVERSITY_NUMBER; i++) {
@@ -21,20 +21,24 @@ struct Com_university {
     return false;
   }
 
-  void Com_university::prepare(User* user) {
+  void Com_university::prepare(User user) {
     this->user = user;
   }
 
   void Com_university::execute() {
     if(user->favorability < -50){
+      imageview(L"dead_eyes.bgra");
       printfs(L"I'm nothing to say to you.\n\r");
     } else if(user->favorability >= -50 && user->favorability < 50){
+      imageview(L"smile.bgra");
       printfs(L"I belong to Shizuoka University.\n\r");
       user->favorability += 10; 
     }else {
+      imageview(L"love.bgra");
       printfs(L"I belong to Shizuoka University.\n\r");
       printfs(L"I'm made as a college assignment.\n\r");
       user->favorability += 10; 
     }
+    imageview(L"default.bgra");
   }
 };
